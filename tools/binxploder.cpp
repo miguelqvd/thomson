23664197f95ac4163d6de0b6fb498e2af3810b9f
Adrien Destugues <pulkomandy@pulkomandy.tk>
2016-03-20 21:03:34 +0000

    binxploder: allow to list only, without dumping to file.
    git-svn-id: svn://localhost/thomson@67 85ae3b6b-dc8f-4344-a89d-598714f2e4e5

--- tools/binxploder.cpp
+++ tools/binxploder.cpp
@@ -12,34 +12,52 @@
 
 int main(int argc, char* argv[])
 {
-	if (argc < 2)
+	bool listOnly = false;
+
+	if (argc < 2 || strcmp(argv[1], "--help") == 0)
 	{
-		fprintf(stderr, "%s file.bin\n", argv[0]);
+		fprintf(stderr,
+			"%s file.bin [-l]\n(-l: list only, don't write sections)\n",
+			argv[0]);
 		return 1;
 	}
+
+	if (argc > 2)
+		listOnly = true; // FIXME use getopt or something
 
 	FILE* f1, * f2;
 	uint8_t i = 0;
 	DECB_Chunk h;
+	char* buf = NULL;
 
 	f1 = fopen(argv[1], "rb");
-	char* buf = (char*)malloc(strlen(argv[1] + 3));
+
+	if (f1 < 0)
+	{
+		perror("Could not open input file");
+		exit(-1);
+	}
+
+	if (!listOnly) 
+		buf = (char*)malloc(strlen(argv[1] + 3));
 
 	while(DECB_ReadChunk(f1, h))
 	{
-		printf("Type: %02x - Size: %04x - Load: %04x\n", h.type, h.length, h.addr);
+		printf("Type: %02x - Size: %04x - %s: %04x\n", h.type, h.length,
+			h.type == 0xFF ? "Exec" : "Load", h.addr);
 
-		sprintf(buf, "%s.%02x", argv[1], i++);
-		f2 = fopen(buf, "wb");
+		if (!listOnly)
+		{
+			sprintf(buf, "%s.%02x", argv[1], i++);
+			f2 = fopen(buf, "wb");
 
-		fwrite(&h.data[0], h.length, 1, f2);
+			fwrite(&h.data[0], h.length, 1, f2);
 
-		fclose(f2);
+			fclose(f2);
+		}
 	}
 
 	free(buf);
-
 	fclose(f1);
-
 	return 0;
 }
