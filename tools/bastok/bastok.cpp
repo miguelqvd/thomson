844e7583c515439b430e11efc4e02cc6a8bcae7b
Adrien Destugues <pulkomandy@gmail.com>
2018-03-21 21:04:26 +0100

    bastok: add more tokens and support TO9 BASIC

--- tools/bastok/bastok.cpp
+++ tools/bastok/bastok.cpp
@@ -50,13 +50,22 @@ void BAStoASCII(BAS_Data& data)
 	Opcodes[0x89] = "IF";
 	Opcodes[0x8A] = "RESTORE";
 	Opcodes[0x8B] = "RETURN";
+										Opcodes[0x8D] = "'";
+										Opcodes[0x8E] = "STOP";
+										Opcodes[0x8F] = "ELSE"; // FIXME must remove the : before it
 	//
 	Opcodes[0x93] = "DEFINT";
+										Opcodes[0x96] = "ON";
+										Opcodes[0x98] = "ERROR";
+										Opcodes[0x99] = "RESUME";
 	// CURSOR
 	Opcodes[0x9C] = "LOCATE";
 	Opcodes[0x9D] = "CLS";
+										Opcodes[0x9E] = "CONSOLE";
+										Opcodes[0x9F] = "PSET";
 	// MACHINE
 	Opcodes[0xA2] = "EXEC";
+										Opcodes[0xA3] = "BEEP";
 	// DISPLAY
 	Opcodes[0xA4] = "COLOR";
 	Opcodes[0xA5] = "LINE";
@@ -69,17 +78,27 @@ void BAStoASCII(BAS_Data& data)
 	//
 	Opcodes[0xAE] = "CLEAR";
 
+										Opcodes[0xB0] = "KEY";
+										Opcodes[0xB3] = "LOAD";
+										Opcodes[0xB5] = "OPEN";
+										Opcodes[0xB6] = "CLOSE";
+										Opcodes[0xB7] = "INPEN";
 	// SOUND
 	Opcodes[0xB9] = "PLAY";
 
 	// SUB-CODES
+										Opcodes[0xBA] = "TAB(";
 	Opcodes[0xBB] = "TO";
 	Opcodes[0xBC] = "SUB";
+										Opcodes[0xBD] = "FN";
 	//
+	Opcodes[0xBE] = "SPC(";
 	Opcodes[0xBF] = "USING";
 	//
+										Opcodes[0xC1] = "ERL";
+										Opcodes[0xC2] = "ERR";
 	Opcodes[0xC4] = "THEN";
-	//
+										Opcodes[0xC5] = "NOT";
 	Opcodes[0xC6] = "STEP";
 
 	// OPERATORS
@@ -90,31 +109,67 @@ void BAStoASCII(BAS_Data& data)
 	//
 	Opcodes[0xCC] = "AND";
 	Opcodes[0xCD] = "OR";
+	Opcodes[0xCE] = "XOR";
 	//
+										Opcodes[0xD1] = "MOD";
+										Opcodes[0xD2] = "@";
 	Opcodes[0xD3] = ">";
 	Opcodes[0xD4] = "=";
 	Opcodes[0xD5] = "<";
+										Opcodes[0xD6] = "DSKINI";
+										Opcodes[0xDD] = "PUT";
+										Opcodes[0xDE] = "GET";
+										Opcodes[0xE6] = "COPY";
+										Opcodes[0xEF] = "DO";
+										Opcodes[0xF0] = "LOOP";
+										Opcodes[0xF1] = "EXIT";
+										Opcodes[0xF8] = "TURTLE";
 
 
 	Functions[0x81] = "INT";
 	Functions[0x82] = "ABS";
+										Functions[0x8A] = "PEEK";
 	Functions[0x8B] = "LEN";
+										Functions[0x8D] = "VAL";
 	Functions[0x8E] = "ASC";
 	Functions[0x8F] = "CHR$";
+										Functions[0x95] = "HEX$";
 	Functions[0x99] = "GR$";
 	Functions[0x9A] = "LEFT$";
 	Functions[0x9C] = "MID$";
+										Functions[0x9B] = "RIGHT$";
+										Functions[0x9E] = "VARPTR";
 	Functions[0x9F] = "RND";
 	Functions[0xA0] = "INKEY$";
 	Functions[0xA1] = "INPUT$";
+										Functions[0xA2] = "CSRLIN";
 	Functions[0xA4] = "SCREEN";
+										Functions[0xA5] = "POS";
+										Functions[0xA6] = "PTRIG";
+										Functions[0xB0] = "SPACE$";
+										Functions[0xB1] = "STRING$";
+										Functions[0xB2] = "DSKI$";
+										Functions[0xB3] = "FKEY$";
+										Functions[0xBA] = "PALETTE";
+										Functions[0xBB] = "BANK";
+										Functions[0xBC] = "HEAD";
+										Functions[0xBD] = "ROT";
+										Functions[0xBE] = "SHOW";
+										Functions[0xBF] = "ZOOM";
 
 	/* ################################ */
 
 	BAS_Line* currentLine;
+	bool mo5mode;
 	currentLine = (BAS_Line*)&data.data[0];
 
+	// MO5 BASIC uses absolute addresses in the "offset" field. TO8 BASIC 512
+	// instead puts the line length there, making the file relocatable. We try
+	// to guess which format was used here
+	mo5mode = (currentLine->offset & 0xFF) > 0x24;
+
 	bool func = false;
+	int acc = 0;
 	while(currentLine->offset) {
 		// Endianness fixup
 		currentLine->offset = ntohs(currentLine->offset);
@@ -124,7 +179,28 @@ void BAStoASCII(BAS_Data& data)
 		
 		for (int i = 0; currentLine->data[i]; i++) {
 			uint8_t c = currentLine->data[i];
-			if (c < 128 && isprint(c))
+			if (acc != 0) {
+				if (acc < 0) {
+					acc = c;
+					if (c == 0x2E) {
+						printf("→");
+						acc = 0;
+						// TODO handle other special characters...
+					}
+				} else switch((acc << 8) | c) {
+					case 'Be':
+						printf("é");
+						acc = 0;
+						break;
+					default:
+						printf("\nUNKNOWN ACCENT SEQUENCE 16 %02X %02X\n", acc, c);
+						exit(EXIT_FAILURE);
+				}
+				continue;
+			} else if (c == 0x16) {
+				acc = -1;
+				continue;
+			} if (c < 128 && isprint(c))
 				putchar(c);
 			else if (c == 0xFF) {
 				func = true;
@@ -142,7 +218,8 @@ void BAStoASCII(BAS_Data& data)
 			}
 			func = false;
 		}
-		currentLine = (BAS_Line*)&data.data[currentLine->offset - 0x25A4];
+		//currentLine = (BAS_Line*)&data.data[currentLine->offset - 0x25A4];
+		currentLine = (BAS_Line*)((uint8_t*)currentLine + currentLine->offset);
 		puts("");
 	}
 }
