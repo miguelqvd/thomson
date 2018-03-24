1ac7849a73fab01effbd6b741a03884621da18b3
Adrien Destugues <pulkomandy@pulkomandy.tk>
2018-03-24 08:18:54 +0100

    Add reimplementation of sd2fd and fd2sd.
    I don't want to be running Windows tools with annoying dialog boxes!

---
+++ tools/sd2fd.cpp
@@ -0,0 +1,59 @@
+/*
+ * fd2sd.c
+ * Copyright (C) 2018 pulkomandy <pulkomandy@kitt>
+ *
+ * Converts FD files to SD, for use with SDMOTO
+ * Essentially this just pads 256 byte sectors to 512 bytes.
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char** argv)
+{
+	if (argc < 3 || strcmp(argv[1], "--help") == 0)
+	{
+		fprintf(stderr,
+			"%s infile outfile\n",
+			argv[0]);
+		return 1;
+	}
+
+	FILE* f1;
+   	FILE* f2;
+
+	f1 = fopen(argv[1], "rb");
+
+	if (f1 < 0) {
+		perror("Could not open input file");
+		exit(-1);
+	}
+
+	f2 = fopen(argv[2], "wb");
+
+	if (f2 < 0) {
+		perror("Could not open output file");
+		exit(-2);
+	}
+
+	char buffer[256];
+	int i = 0;
+
+	for (;;) {
+		int v = fread(buffer, 1, 256, f1);
+		if (v != 256)
+			break;
+		fwrite(buffer, 1, v, f2);
+
+		printf("Wrote %d bytes in sector %d.\n", v, i++);
+
+		// skip next 256 bytes (padding)
+		v = fread(buffer, 1, 256, f1);
+	}
+
+	return 0;
+}
